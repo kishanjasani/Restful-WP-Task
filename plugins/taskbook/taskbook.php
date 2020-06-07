eb8dcb695b6300df147896fb5d68021fc53f6416
kishan <kishanjasani007@yahoo.in>
2020-06-07 14:57:29 +0530

    Add Taskbook plugin initialization

---
+++ plugins/taskbook/taskbook.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Task Book
+Plugin URI:  https://kishanjasani.github.com/profile
+Description: Tract stress and anxiety levels around tasks.
+Version:     1.0
+Author:      Kishan Jasani
+Author URI:  https://kishanjasani.github.com/profile
+Text Domain: taskbook
+Domain Path: /languages
+License:     GPL3
+
+Task Book is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Task Book is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Task Book. If not, see http://www.gnu.org/licenses/gpl-3.0.html.
+*/
